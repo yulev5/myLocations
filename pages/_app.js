f5ea4370f9513525b0ee94edc77c686b5ca437a3
Yuval <yulev5@gmail.com>
2021-04-28 16:18:23 +0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../styles/theme';
+import '../styles/reset.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
